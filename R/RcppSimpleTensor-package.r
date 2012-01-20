083bd3731381a5edddfdcfab96ff8372ee938334
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2012-01-20 18:23:40 -0500

    changed the structure of the package, using roxygen for documentation

---
+++ R/RcppSimpleTensor-package.r
@@ -0,0 +1,11 @@
+#' C++ tensor expressions made easy and fast 
+#'
+#' The goal of this library is to help create cpp implementations for multidimensional operations.
+#' Use the main 2 functions RcppSimpleTensor and TI to get started.
+#' ...
+#' 
+#' @import Rcpp 
+#' @docType package
+#' @name RcppSimpleTensor
+#' @aliases RcppSimpleTensor RcppSimpleTensor-package
+NULL
